8d880f9cb20648a8e2649d55ba3421f1ff6ac4e8
Fabio Queiroz <fabiovianaqueiroz@gmail.com>
2021-03-06 01:09:30 +0000

    basic routing

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Home Page Test</h1>
+      <p>test content</p>
+      <a href="/about">About</a>
+    </div>
+  );
+}
+
+export default HomePage;
